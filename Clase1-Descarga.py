81120f25a49ee4fcd1c04d8cab1967183f08be94
LPedraza <lupipedraza@gmail.com>
2021-05-12 11:53:22 -0300

    actualizar clase

--- Clase1-Descarga.py
+++ Clase1-Descarga.py
@@ -12,38 +12,39 @@ Importamos una librería que se llama codigo_General que tiene todas las funcion
 
 El código a continuación está armado como ejemplo, pero la idea es que puedan entenderlo para poder usarlo, modificarlo, copiar partes, etc.
 """
+
+
 import importlib.util
 import os
 
-path = '/home/tcicchini/Documentos/Cursos/Curso_Twitter/' # Este es el directorio donde está alojado el archivo codigo_General.py
+path = '/home/lucia/Escritorio/Curso Redes/Codigos2.0/Curso_Twitter/' # Este es el directorio donde está alojado el archivo codigo_General.py
 
-#Si estamos en la carpeta correcta podemos importar codigo_General import codigo_General
+# Si estamos en la carpeta correcta podemos importar codigo_General import codigo_General
 
-#Para evitar problemas de carpeta podemos en cambio hacer:
+# Para evitar problemas de carpeta podemos en cambio hacer:
 spec = importlib.util.spec_from_file_location('codigo_General', os.path.join(path,'codigo_General.py'))
 codigo_General = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(codigo_General)
 
 
-path_proyecto = '/home/tcicchini/Documentos/Cursos/Curso_Twitter/Prueba/' # Este es el nombre del directorio donde está el proyecto.
-
- #Lugar donde guardaremos todos los archivos de texto (desde la carpeta desde donde estamos corriendo)
-Tema='Biden' # (Opcional) Nombre con el que quiero identificar los archivos de este tema
-Archivo_Tweets=Tema+'_tweets.txt' #Acá armamos el nombre del archivo donde se van a descargar los tweets
-Cantidad=50 #Cantidad de tweets que desceamos para los métodos al pasado 
-idioma=['es'] #Idiomas de los tweets que buscamos ('en' si es inglés)
-
-palabras=['Biden'] #Palabras que buscamos para el método por palabras en stream o al pasadp
-usuario=['CFKArgentina'] #Usuarios que buscamos para el método por usuarios
 
 
+# Definimos el conjunto de palabras para realizar la desarga
 
+palabras = ['Alberto Fernandez', 'FMI', 'Club de Paris'] # Defino conjunto de palabras
+path_guardado = '/home/tcicchini/Documentos/Cursos/busqueda_12_05_2021' # Defino a dónde quiero que se guarden los archivos
+archivo_guardado = 'descarga_12_05_2121.txt' # El nombre del archivo para que se guarden los tweets
 
-#Podemos elegir estos 3 tipos de descargas
+# Inicializamos la descarga en vivo con el método Descargar_por_palabra_stream
+codigo_General.Descargar_por_palabra_stream(Palabras = palabras, # Le pasamos las palabras para filtrar
+                                            Archivo_Tweets = os.path.join(path_guardado,
+                                                                          archivo_guardado), # Le pasamos la ubicación y nombre de archivo de destino
+                                            idioma = ['es'], # indicamos el idioma
+                                            ) 
 
-codigo_General.Descargar_por_palabra_stream(palabras,path+Archivo_Tweets,idioma)
+#Otras opciones
 
-codigo_General.Descargar_por_palabras(palabras,path+Archivo_Tweets,Cantidad)
+#codigo_General.Descargar_por_palabras(palabras,path+Archivo_Tweets,Cantidad)
 
-codigo_General.Descargar_por_usuarios(usuario,path+Archivo_Tweets,Cantidad)
+#codigo_General.Descargar_por_usuarios(usuario,path+Archivo_Tweets,Cantidad)
 
