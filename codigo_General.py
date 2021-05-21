2a76ddb8986a88aacad4ffe4de4c356e375b53c3
tcicchini <tomas.cicchini@gmail.com>
2021-05-21 16:14:33 -0300

    sumé follow como parámetro de búsqueda

--- codigo_General.py
+++ codigo_General.py
@@ -124,7 +124,7 @@ class TwitterStreamer():
     def __init__(self):
         pass
 
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_list = '', languages = ['es']):
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list = '', languages = ['es'], follow = None):
         # This handles Twitter authetification and the connection to Twitter Streaming API
         listener = StdOutListener(fetched_tweets_filename)
         auth = OAuthHandler(lector_claves()[0], lector_claves()[1])
@@ -135,6 +135,7 @@ class TwitterStreamer():
         if len(hash_tag_list) != 0:
             stream.filter(languages = languages,
                           track = hash_tag_list,
+                          follow = follow,
                           )
         else:
             stream.sample(languages = languages)
